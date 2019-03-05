91878c8ca2f7510f64e5afb8e32853850e01b434
Raghava Lakshminarayana <raglaks94@gmail.com>
2019-03-05 13:32:56 -0600

    add env file to protect api keys

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 const Twit = require('twit');
+const dotenv = require('dotenv');
+dotenv.config({path: '/Users/raglaks/Desktop/PROJECTS/la_cuarta/.env.juicy'});
 
 const T = new Twit({
 
-	consumer_key: 'ctHInIMBhU4ydVaNc3xmtmrG9',
-	consumer_secret: 'muDQX4eGKfm6xXjxzhjE0QaIdc3CEMyjTzY6iHNvQlBUU9M5BD',
-	access_token: '88828925-bS0OU3KsMoj2DsR1QAXGkpRF6s9FvHTuUo1tNSfKS',
-	access_token_secret: 'sB03ysQRwbXKYAf0HEAFwbtmGUwD9pUSCxm1f7ykNoJxo'
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET,
+	access_token: process.env.ACCESS_TOKEN,
+	access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 
 });
 
